53900dfcce95983582e35b68450568a17d7bcda4
Mihir Chakkarwar <mchakkarwar28@gmail.com>
2021-06-01 17:55:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Statewise from './component/stateWiseData/statewise';
+import './component/stateWiseData/statewise.css';
+
+const App = () => {
+  return(
+    <>
+      <h1> <Statewise/> </h1>
+    </>
+  );
+};
+
+export default App;
